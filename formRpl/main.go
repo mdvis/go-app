2dc453b568cfed1be49ed1057c88ecc95f443cac
dewei.ma <xnmageis@outlook.com>
2022-11-16 18:29:42 +0800

    chore: form

--- formRpl/main.go
+++ formRpl/main.go
@@ -11,28 +11,80 @@ import (
 )
 
 var wg sync.WaitGroup
+var dt = make(chan []string, 225)
+var a, b int
 
 func main() {
-	list := os.Args[1:]
+	listPath := os.Args[1]
 
-	var echo chan []string = make(chan []string, len(list))
-
-	for _, v := range list {
+	readFile(listPath, func(s string) {
 		wg.Add(1)
-		go readFile(v, echo)
-	}
+		readLine(s, handleFile)
+	})
 
-	go func() {
-		for k := range echo {
-			fmt.Println(k)
-			wg.Done()
-		}
-	}()
+	for i := range dt {
+		wg.Done()
+		a++
+		fmt.Println(a, "---------------------------------------------", i,
+			"==========================================")
+	}
 
 	wg.Wait()
 }
 
-func readFile(path string, data chan<- []string) {
+func handleFile(s string) string {
+	go readFile(s, func(s string) {
+		wg.Add(1)
+		readLine(s, replaceKeyWord, dt)
+	})
+	return ""
+}
+
+func replaceKeyWord(s string) string {
+	formIdx := strings.Index(s, "Form")
+	importIdx := strings.Index(s, "import")
+	hasForm := formIdx != -1
+	hasImport := importIdx != -1
+
+	if hasForm && hasImport {
+		return strings.Replace(s, "Form", "", 1)
+	}
+	return s
+}
+
+type CB func(string)
+type RCB func(string) string
+type DT chan<- []string
+
+func readLine(file string, callback RCB, data ...DT) {
+	var d DT
+	var l []string
+	if len(data) != 0 {
+		d = data[0]
+	}
+	stringReader := strings.NewReader(file)
+	reader := bufio.NewReader(stringReader)
+	for {
+		line, _, err := reader.ReadLine()
+
+		if err != nil {
+			break
+		}
+		if d != nil {
+			l = append(l, callback(string(line)))
+		}
+		callback(string(line))
+	}
+	if d != nil {
+		wg.Add(1)
+		b++
+		fmt.Println("b", b)
+		d <- l
+	}
+	wg.Done()
+}
+
+func readFile(path string, callback CB) {
 	f, err := ioutil.ReadFile(path)
 
 	if err != nil {
@@ -40,32 +92,5 @@ func readFile(path string, data chan<- []string) {
 	}
 
 	content := string(f)
-	readLine(content, data)
-
-	fmt.Println(path)
-}
-
-func readLine(file string, data chan<- []string) {
-	stringReader := strings.NewReader(file)
-	reader := bufio.NewReader(stringReader)
-	lineList := []string{}
-
-	for {
-		line, _, err := reader.ReadLine()
-
-		formIdx := strings.Index(string(line), "form")
-		hasForm := formIdx != -1
-		if hasForm {
-			fmt.Println(hasForm)
-		}
-		fmt.Println()
-
-		if err != nil {
-			break
-		}
-
-		lineList = append(lineList, string(line))
-	}
-
-	data <- lineList
+	callback(content)
 }
